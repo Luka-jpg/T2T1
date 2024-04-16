ff284b41e984b2bb6eea797c2d7812da4168bf4d
unknown <sve.luka.sco@gmail.com>
2024-04-16 19:16:22 -0300

    cambios

--- clientesqa.tf
+++ clientesqa.tf
@@ -2,20 +2,16 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-    region = "us-east-1"  # Cambiado a us-east-1
+    region = "us-east-1"
     access_key = var.aws_access_key
     secret_key = var.aws_secret_key
 }
 
-data "aws_security_group" "existing" {
-    name = "instance_security_group"
-}
-
+# Recurso de grupo de seguridad
 resource "aws_security_group" "instance_security_group" {
-    count = data.aws_security_group.existing.id == null ? 1 : 0
     name = "instance_security_group"
     description = "Security group for EC2 instance"
-    
+
     ingress {
         from_port = 22
         to_port = 22
@@ -31,15 +27,15 @@ resource "aws_security_group" "instance_security_group" {
     }
 
     ingress {
-        from_port = 3000
-        to_port = 3000
+        from_port = 80
+        to_port = 80
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     ingress {
-        from_port = 80
-        to_port = 80
+        from_port = 3000
+        to_port = 3000
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
@@ -52,18 +48,24 @@ resource "aws_security_group" "instance_security_group" {
     }
 }
 
+# Recurso de instancia EC2
 resource "aws_instance" "clientesqa_instance" {
-    ami = "ami-019f9b3318b7155c5"  # AMI de Amazon Linux
+    ami = "ami-0a3c3a20c09d6f377"  # AMI de Amazon Linux
     instance_type = "t2.micro"  # Tipo de instancia
-    key_name = "clavepem"  # Nombre de la clave de acceso existente en AWS
-    
-    vpc_security_group_ids = coalesce([data.aws_security_group.existing.id], length(aws_security_group.instance_security_group) > 0 ? [aws_security_group.instance_security_group[0].id] : [])
+    key_name = "clavepem"  # Nombre de tu clave de acceso existente en AWS
 
+    # Asociar la instancia con el grupo de seguridad creado
+    vpc_security_group_ids = [
+        aws_security_group.instance_security_group.id
+    ]
+
+    # Etiquetas para identificar la instancia
     tags = {
-        Name = "ORG-CLIENTES-QA"  # Nombre de la instancia
+        Name = "ORG-CLIENTES-QA"
     }
 }
 
+# Bloque de salida para obtener la dirección IP pública de la instancia
 output "instance_ip_clientesqa" {
     value = aws_instance.clientesqa_instance.public_ip
 }
